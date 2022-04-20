b9e16133bedc28737aca18fac03b2e46877ff777
iafs32 <iafs.0232@gmail.com>
2022-04-20 08:58:35 -0500

    Se hizo el diseño del inventario

---
+++ Tienda de Abarrotes/frmInventario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tienda_de_Abarrotes
+{
+    public partial class frmInventario : Form
+    {
+        public frmInventario()
+        {
+            InitializeComponent();
+        }
+
+        private void frmInventario_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'tiendaDeAbarrotesDataSet.Inventario' Puede moverla o quitarla según sea necesario.
+            this.inventarioTableAdapter.Fill(this.tiendaDeAbarrotesDataSet.Inventario);
+
+        }
+    }
+}
