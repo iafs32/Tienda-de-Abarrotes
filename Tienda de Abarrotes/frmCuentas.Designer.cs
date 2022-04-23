bd27a0caf6a7439830dd51ee623cef1ab89e5a02
iafs32 <iafs.0232@gmail.com>
2022-04-23 11:56:49 -0500

    Se hizo el diseño del forms cuentas

---
+++ Tienda de Abarrotes/frmCuentas.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Tienda_de_Abarrotes
+{
+    partial class frmCuentas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dgvCuentas = new System.Windows.Forms.DataGridView();
+            this.tiendaDeAbarrotesDataSet = new Tienda_de_Abarrotes.tiendaDeAbarrotesDataSet();
+            this.bancosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.bancosTableAdapter = new Tienda_de_Abarrotes.tiendaDeAbarrotesDataSetTableAdapters.BancosTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fechaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnCaja = new System.Windows.Forms.Button();
+            this.btnBancos = new System.Windows.Forms.Button();
+            this.btnVentas = new System.Windows.Forms.Button();
+            this.btnCompras = new System.Windows.Forms.Button();
+            this.lblCuenta = new System.Windows.Forms.Label();
+            this.cajaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cajaTableAdapter = new Tienda_de_Abarrotes.tiendaDeAbarrotesDataSetTableAdapters.CajaTableAdapter();
+            this.comprasBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.comprasTableAdapter = new Tienda_de_Abarrotes.tiendaDeAbarrotesDataSetTableAdapters.ComprasTableAdapter();
+            this.ventasBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ventasTableAdapter = new Tienda_de_Abarrotes.tiendaDeAbarrotesDataSetTableAdapters.VentasTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCuentas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tiendaDeAbarrotesDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bancosBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cajaBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comprasBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ventasBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCuentas
+            // 
+            this.dgvCuentas.AutoGenerateColumns = false;
+            this.dgvCuentas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCuentas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.fechaDataGridViewTextBoxColumn,
+            this.valorDataGridViewTextBoxColumn});
+            this.dgvCuentas.DataSource = this.cajaBindingSource;
+            this.dgvCuentas.Location = new System.Drawing.Point(12, 44);
+            this.dgvCuentas.Name = "dgvCuentas";
+            this.dgvCuentas.RowHeadersWidth = 62;
+            this.dgvCuentas.RowTemplate.Height = 28;
+            this.dgvCuentas.Size = new System.Drawing.Size(514, 243);
+            this.dgvCuentas.TabIndex = 0;
+            // 
+            // tiendaDeAbarrotesDataSet
+            // 
+            this.tiendaDeAbarrotesDataSet.DataSetName = "tiendaDeAbarrotesDataSet";
+            this.tiendaDeAbarrotesDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bancosBindingSource
+            // 
+            this.bancosBindingSource.DataMember = "Bancos";
+            this.bancosBindingSource.DataSource = this.tiendaDeAbarrotesDataSet;
+            // 
+            // bancosTableAdapter
+            // 
+            this.bancosTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // fechaDataGridViewTextBoxColumn
+            // 
+            this.fechaDataGridViewTextBoxColumn.DataPropertyName = "Fecha";
+            this.fechaDataGridViewTextBoxColumn.HeaderText = "Fecha";
+            this.fechaDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.fechaDataGridViewTextBoxColumn.Name = "fechaDataGridViewTextBoxColumn";
+            this.fechaDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // valorDataGridViewTextBoxColumn
+            // 
+            this.valorDataGridViewTextBoxColumn.DataPropertyName = "Valor";
+            this.valorDataGridViewTextBoxColumn.HeaderText = "Valor";
+            this.valorDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.valorDataGridViewTextBoxColumn.Name = "valorDataGridViewTextBoxColumn";
+            this.valorDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // btnCaja
+            // 
+            this.btnCaja.Location = new System.Drawing.Point(12, 297);
+            this.btnCaja.Name = "btnCaja";
+            this.btnCaja.Size = new System.Drawing.Size(105, 35);
+            this.btnCaja.TabIndex = 1;
+            this.btnCaja.Text = "Caja";
+            this.btnCaja.UseVisualStyleBackColor = true;
+            // 
+            // btnBancos
+            // 
+            this.btnBancos.Location = new System.Drawing.Point(123, 297);
+            this.btnBancos.Name = "btnBancos";
+            this.btnBancos.Size = new System.Drawing.Size(105, 35);
+            this.btnBancos.TabIndex = 2;
+            this.btnBancos.Text = "Bancos";
+            this.btnBancos.UseVisualStyleBackColor = true;
+            // 
+            // btnVentas
+            // 
+            this.btnVentas.Location = new System.Drawing.Point(234, 297);
+            this.btnVentas.Name = "btnVentas";
+            this.btnVentas.Size = new System.Drawing.Size(105, 35);
+            this.btnVentas.TabIndex = 3;
+            this.btnVentas.Text = "Ventas";
+            this.btnVentas.UseVisualStyleBackColor = true;
+            // 
+            // btnCompras
+            // 
+            this.btnCompras.Location = new System.Drawing.Point(345, 297);
+            this.btnCompras.Name = "btnCompras";
+            this.btnCompras.Size = new System.Drawing.Size(105, 35);
+            this.btnCompras.TabIndex = 4;
+            this.btnCompras.Text = "Compras";
+            this.btnCompras.UseVisualStyleBackColor = true;
+            // 
+            // lblCuenta
+            // 
+            this.lblCuenta.AutoSize = true;
+            this.lblCuenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCuenta.Location = new System.Drawing.Point(12, 9);
+            this.lblCuenta.Name = "lblCuenta";
+            this.lblCuenta.Size = new System.Drawing.Size(98, 32);
+            this.lblCuenta.TabIndex = 5;
+            this.lblCuenta.Text = "label1";
+            // 
+            // cajaBindingSource
+            // 
+            this.cajaBindingSource.DataMember = "Caja";
+            this.cajaBindingSource.DataSource = this.tiendaDeAbarrotesDataSet;
+            // 
+            // cajaTableAdapter
+            // 
+            this.cajaTableAdapter.ClearBeforeFill = true;
+            // 
+            // comprasBindingSource
+            // 
+            this.comprasBindingSource.DataMember = "Compras";
+            this.comprasBindingSource.DataSource = this.tiendaDeAbarrotesDataSet;
+            // 
+            // comprasTableAdapter
+            // 
+            this.comprasTableAdapter.ClearBeforeFill = true;
+            // 
+            // ventasBindingSource
+            // 
+            this.ventasBindingSource.DataMember = "Ventas";
+            this.ventasBindingSource.DataSource = this.tiendaDeAbarrotesDataSet;
+            // 
+            // ventasTableAdapter
+            // 
+            this.ventasTableAdapter.ClearBeforeFill = true;
+            // 
+            // frmCuentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 344);
+            this.Controls.Add(this.lblCuenta);
+            this.Controls.Add(this.btnCompras);
+            this.Controls.Add(this.btnVentas);
+            this.Controls.Add(this.btnBancos);
+            this.Controls.Add(this.btnCaja);
+            this.Controls.Add(this.dgvCuentas);
+            this.Name = "frmCuentas";
+            this.Text = "Cuentas";
+            this.Load += new System.EventHandler(this.frmCuentas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCuentas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tiendaDeAbarrotesDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bancosBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cajaBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comprasBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ventasBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCuentas;
+        private tiendaDeAbarrotesDataSet tiendaDeAbarrotesDataSet;
+        private System.Windows.Forms.BindingSource bancosBindingSource;
+        private tiendaDeAbarrotesDataSetTableAdapters.BancosTableAdapter bancosTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fechaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn valorDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button btnCaja;
+        private System.Windows.Forms.Button btnBancos;
+        private System.Windows.Forms.Button btnVentas;
+        private System.Windows.Forms.Button btnCompras;
+        private System.Windows.Forms.Label lblCuenta;
+        private System.Windows.Forms.BindingSource cajaBindingSource;
+        private tiendaDeAbarrotesDataSetTableAdapters.CajaTableAdapter cajaTableAdapter;
+        private System.Windows.Forms.BindingSource comprasBindingSource;
+        private tiendaDeAbarrotesDataSetTableAdapters.ComprasTableAdapter comprasTableAdapter;
+        private System.Windows.Forms.BindingSource ventasBindingSource;
+        private tiendaDeAbarrotesDataSetTableAdapters.VentasTableAdapter ventasTableAdapter;
+    }
+}
---
+++ Tienda de Abarrotes/frmCuentas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tienda_de_Abarrotes
+{
+    public partial class frmCuentas : Form
+    {
+        public frmCuentas()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCuentas_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'tiendaDeAbarrotesDataSet.Ventas' Puede moverla o quitarla según sea necesario.
+            this.ventasTableAdapter.Fill(this.tiendaDeAbarrotesDataSet.Ventas);
+            // TODO: esta línea de código carga datos en la tabla 'tiendaDeAbarrotesDataSet.Compras' Puede moverla o quitarla según sea necesario.
+            this.comprasTableAdapter.Fill(this.tiendaDeAbarrotesDataSet.Compras);
+            // TODO: esta línea de código carga datos en la tabla 'tiendaDeAbarrotesDataSet.Caja' Puede moverla o quitarla según sea necesario.
+            this.cajaTableAdapter.Fill(this.tiendaDeAbarrotesDataSet.Caja);
+            // TODO: esta línea de código carga datos en la tabla 'tiendaDeAbarrotesDataSet.Bancos' Puede moverla o quitarla según sea necesario.
+            this.bancosTableAdapter.Fill(this.tiendaDeAbarrotesDataSet.Bancos);
+
+            lblCuenta.Text = "Caja: $" + ;
+
+        }
+    }
+}
